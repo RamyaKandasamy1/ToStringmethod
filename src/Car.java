bc4ff9bc59b551c481e538a5038f2e08f264f75f
RamyaKandasamy1 <ramz87lakshmi@yahoo.co.in>
2022-10-06 00:03:13 -0500

    Second commit

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+
+public class Car {
+	
+	String name="Honda";
+	String model="CRV";
+	int year =2022;
+	String colour ="Black";
+	
+	public String toString() {
+		return colour +"\n"+name+"\n"+model+"\n"+year;
+		
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car mycar=new Car();
+		
+		//Sout(mycar);// will return the address of the mycarobject and not its value
+		//to access the value we ve to ovverwrite the toString method in the car class
+		
+		System.out.println(mycar);
+
+	}
+
+}
